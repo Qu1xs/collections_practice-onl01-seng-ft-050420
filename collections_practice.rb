aab0cabb6d6d8eb9d3b425a2fd2a2291159b3a59
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-05 21:07:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,42 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  temp = array.sort{|a, b| a.length <=> b.length}
+  return temp
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+end
+
+def reverse_array(array)
+  return array.reverse
+end
+
+def kesha_maker(array)
+  array.each {|i|  }
+end
+
+def find_a(array)
+  array.find_all {|i| i.start_with?"a" }
+end
+
+def sum_array(array)
+  array.inject {|sum, n| sum + n }
+end
+
+def add_s(array)
+  ary = array.each_with_index.collect {|item, i| item + "s" if i != 1}
+  return ary
+end
